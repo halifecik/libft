5242cfc6d8f4d73d9b191854b07823122f5a5555
halifecik <khalifecik@gmail.com>
2025-08-24 15:22:40 +0300

    Minor adjustment to loop logic.

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -5,10 +5,7 @@ void	*ft_memset(void *s, int c, size_t len)
 	size_t	i;
 
 	i = 0;
-	while (i < len)
-	{
-		((unsigned char *)s)[i] = (unsigned char)c;
-		i++;
-	}
+	while (len--)
+		((unsigned char *)s)[i++] = (unsigned char)c;
 	return (s);
 }
