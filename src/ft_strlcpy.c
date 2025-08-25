e29ca43de33ce03da9dca6b3b5b0901f378d7eb6
halifecik <khalifecik@gmail.com>
2025-08-25 12:11:23 +0300

    Minor adjustment.

--- src/ft_strlcpy.c
+++ src/ft_strlcpy.c
@@ -4,14 +4,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 
-	i = 0;
+	i = -1;
 	if (dstsize)
 	{
-		while (src[i] && i < (dstsize - 1))
-		{
+		while (src[++i] && i < (dstsize - 1))
 			dst[i] = src[i];
-			i++;
-		}
 		dst[i] = 0;
 	}
 	return (ft_strlen(src));
