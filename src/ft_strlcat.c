a6bd4c70937f780116d6969dfa7e470ba6f79b2b
halifecik <khalifecik@gmail.com>
2025-08-25 12:09:49 +0300

    Removed dependencies except ft_strlcpy.

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -3,12 +3,15 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	i;
-
+	size_t	len;
 	if (!dest && !size)
 		return (0);
-	i = ft_strlen(dest);
+	while(dest[i])
+		i++;
+	while(src[len])
+		len++;
 	if (i >= size)
-		return (ft_strlen(src) + size);
+		return (len + size);
 	ft_strlcpy((dest + i), src, size - i);
-	return (i + ft_strlen(src));
+	return (len + i);
 }
