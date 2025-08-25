b1ffdebc4c4ef2500c0ed84bffe122523203ccc2
halifecik <khalifecik@gmail.com>
2025-08-25 10:47:28 +0300

    Minor adjustment.

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -2,27 +2,21 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	int		i;
+	int		j;
 	char	*str;
-	size_t	i;
-	size_t	x;
 
-	i = 0;
-	x = 0;
 	if (!s1 || !s2)
 		return (NULL);
 	str = malloc(((ft_strlen(s1) + ft_strlen(s2)) + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
-	while (s1[i])
-	{
+	i = -1;
+	while (s1[++i])
 		str[i] = s1[i];
-		i++;
-	}
-	while (s2[x])
-	{
-		str[i + x] = s2[x];
-		x++;
-	}
-	str[i + x] = '\0';
+	j = -1;
+	while (s2[++j])
+		str[i + j] = s2[j];
+	str[i + j] = '\0';
 	return (str);
 }
