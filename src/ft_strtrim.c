c07be24acf6371ea1c2e361595552ad42e6906d0
halifecik <khalifecik@gmail.com>
2025-08-25 14:03:14 +0300

    Minor adjustment.

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -3,17 +3,19 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
-	char	*trmd;
 	char	*tmp;
+	char	*ret;
 
 	tmp = (char *)s1;
 	if (!tmp || !set)
 		return (NULL);
 	while (*tmp && ft_strchr(set, *tmp))
 		tmp++;
-	i = ft_strlen(tmp);
-	while (i != 0 && ft_strchr(set, tmp[i]))
+	i = 0;
+	while (tmp[i])
+		i++;
+	while (i && ft_strchr(set, tmp[i]))
 		i--;
-	trmd = ft_substr((char *)tmp, 0, (i + 1));
-	return (trmd);
+	ret = ft_substr((char *)tmp, 0, (i + 1));
+	return (ret);
 }
