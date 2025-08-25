79acb2409fe2fec4e1f0dbbaa10b23dc938544b1
halifecik <khalifecik@gmail.com>
2025-08-25 12:14:23 +0300

    Minor adjustment & Added NULL check.

--- src/ft_strmapi.c
+++ src/ft_strmapi.c
@@ -5,17 +5,17 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	char			*rtn;
 
-	if (!s)
+	if (!s || !f)
 		return (NULL);
 	i = 0;
-	rtn = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	while(s[i])
+		i++;
+	rtn = (char *)malloc(sizeof(char) * (i + 1));
 	if (!rtn)
 		return (NULL);
-	while (s[i])
-	{
+	i = -1;
+	while (s[++i])
 		rtn[i] = f(i, s[i]);
-		i++;
-	}
 	rtn[i] = '\0';
 	return (rtn);
 }
