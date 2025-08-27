48f1a941a0fbdd849b09f43e9e8c4f06c6feea3b
halifecik <khalifecik@gmail.com>
2025-08-27 14:06:42 +0300

    Added ft_lstclear function w/ Norm checks.

---
+++ src/bonus/ft_lstclear_bonus.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	if (lst && *lst && del)
+	{
+		while (*lst)
+		{
+			tmp = (*lst)->next;
+			del((*lst)->content);
+			free(*lst);
+			*lst = tmp;
+		}
+		*lst = NULL;
+	}
+}
