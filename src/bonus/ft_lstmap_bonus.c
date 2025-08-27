0bb0c5e401e03e895b8f26d28078e964dbc7beea
halifecik <khalifecik@gmail.com>
2025-08-27 23:44:55 +0300

    Fixed issues in bonus.

--- src/bonus/ft_lstmap_bonus.c
+++ src/bonus/ft_lstmap_bonus.c
@@ -6,6 +6,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 	t_list	*node;
 
+	if (!f || !del)
+		return (NULL);
 	new = NULL;
 	while (lst)
 	{
--- src/bonus/ft_lstsize_bonus.c
+++ src/bonus/ft_lstsize_bonus.c
@@ -5,7 +5,10 @@ int	ft_lstsize(t_list *lst)
 	int	i;
 
 	i = 0;
-	while (lst && i++)
+	while (lst)
+	{
 		lst = lst->next;
+		i++;
+	}
 	return (i);
 }
